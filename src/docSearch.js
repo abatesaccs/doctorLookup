3cfbde7d80b0d1851e928c52c37699e531993496
Andrew <abatesaccs@gmail.com>
2019-11-22 16:02:02 -0800

    return a list of doctors, and their info based on a name search

--- src/docSearch.js
+++ src/docSearch.js
@@ -1,16 +1,19 @@
 export class DocCall{
     async nameCall(name) {
-        try {
-            let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&fields=profile&skip=0&limit=2&user_key=851d52693f5a10ef473cea5ff2e79504`);
-            let jsonifiedResponse = await response.json();
-            return jsonifiedResponse;
-        } catch (error) {
-            console.error("there was an error: " + error.message);
-        }
+      try {
+        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&fields=profile%2Cpractices&skip=0&limit=100&user_key=851d52693f5a10ef473cea5ff2e79504`);
         
+        let jsonifiedResponse = await response.json();
+        return jsonifiedResponse;
+      } catch (error) {
+        $(".errors").html("there was an error: " + error.message);
+      }
     }
+    // async specialtyCall(specialty) {
+    //   try {
+    //     let response = await fetch(``)
+    //   } catch (error) {
+    //     $(".errors").html("there was an error: " + error.message);
+    //   }
+    // }
 }
-// const getElements = function(response) {
-//     return (`${response.data[0].profile.last_name}`);
-//     // (`${response.data.profile.last_name}`);
-// };
--- src/main.js
+++ src/main.js
@@ -8,17 +8,31 @@ $(document).ready(function() {
     $('#inputButton').click(function(event) {
       event.preventDefault();
       let input = $("#nameInput").val();
+      let choice = $("#searchMethod").val();
       (async () => {
         let docCall = new DocCall();
-        const response = await docCall.nameCall(input);
-        return getElements(response)
+        if (choice === "false") {
+          $(".errors").html("Please select a search method.");
+          return;
+        } else if (choice === "first") {
+          const response = await docCall.nameCall(input);
+          return getNames(response);
+        } else if (choice === "specialty") {
+          const response = await docCall.specialtyCall(input);
+          return getNames(response);
+        }
+        
       })();
 
-      function getElements(response) {
-        for (let i = 0; i < response.data.length; i++) {
-          $(".showName").append(`Name: ${response.data[i].profile.last_name}.`);
+      function getNames(response) {
+        if (response.data.length > 1) {
+          $(".showInfo").append("Result: <br/>");
+          for (let i = 0; i < response.data.length; i++) {
+            $(".showInfo").append(`${response.data[i].profile.first_name} ${response.data[i].profile.last_name} <br/> ${response.data[i].practices[0].visit_address.street} <br/> ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br/> Phone number: ${response.data[i].practices[0].phones[0].number} <br/> Accepting new patients: ${response.data[i].practices[0].accepts_new_patients} <br/><br/>`);
+          }
+        } else {
+          return 1;
         }
       }
-
     });
 });
