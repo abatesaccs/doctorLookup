2873bf10827aecaf331b290833e60e848b8b05b2
Andrew <abatesaccs@gmail.com>
2019-11-22 16:23:00 -0800

    return a list of doctors based on one of four search methods

--- src/docSearch.js
+++ src/docSearch.js
@@ -1,19 +1,45 @@
 export class DocCall{
-    async nameCall(name) {
-      try {
-        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&fields=profile%2Cpractices&skip=0&limit=100&user_key=851d52693f5a10ef473cea5ff2e79504`);
-        
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-      } catch (error) {
-        $(".errors").html("there was an error: " + error.message);
-      }
+  async firstCall(name) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?first_name=${name}&location=or-portland&fields=profile%2Cpractices&skip=0&limit=100&user_key=851d52693f5a10ef473cea5ff2e79504`);
+      
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      $(".errors").html("there was an error: " + error.message);
     }
-    // async specialtyCall(specialty) {
-    //   try {
-    //     let response = await fetch(``)
-    //   } catch (error) {
-    //     $(".errors").html("there was an error: " + error.message);
-    //   }
-    // }
+  }
+
+  async symptomCall(symptom) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&fields=profile%2Cpractices&skip=0&limit=100&user_key=851d52693f5a10ef473cea5ff2e79504`);
+      
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      $(".errors").html("there was an error: " + error.message);
+    }
+  }
+
+  async lastCall(last) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${last}&location=or-portland&fields=profile%2Cpractices&skip=0&limit=100&user_key=851d52693f5a10ef473cea5ff2e79504`);
+      
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      $(".errors").html("there was an error: " + error.message);
+    }
+  }
+
+  async specialtyCall(specialty) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=or-portland&fields=profile%2Cpractices&skip=0&limit=100&user_key=851d52693f5a10ef473cea5ff2e79504`);
+      
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      $(".errors").html("there was an error: " + error.message);
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { DocCall } from "./docSearch.js";
 $(document).ready(function() {
     $('#inputButton').click(function(event) {
       event.preventDefault();
+      $(".showInfo").html("");
       let input = $("#nameInput").val();
       let choice = $("#searchMethod").val();
       (async () => {
@@ -15,10 +16,21 @@ $(document).ready(function() {
           $(".errors").html("Please select a search method.");
           return;
         } else if (choice === "first") {
-          const response = await docCall.nameCall(input);
+          $(".errors").html("");
+          $(".showInfo").append("Searching");
+          const response = await docCall.firstCall(input);
           return getNames(response);
         } else if (choice === "specialty") {
+          $(".errors").html("");
           const response = await docCall.specialtyCall(input);
+          return getNames(response);
+        } else if (choice === "last") {
+          $(".errors").html("");
+          const response = await docCall.lastCall(input);
+          return getNames(response);
+        } else if (choice === "symptom") {
+          $(".errors").html("");
+          const response = await docCall.symptomCall(input);
           return getNames(response);
         }
         
@@ -26,12 +38,15 @@ $(document).ready(function() {
 
       function getNames(response) {
         if (response.data.length > 1) {
-          $(".showInfo").append("Result: <br/>");
+          $(".showInfo").append("Result: ");
           for (let i = 0; i < response.data.length; i++) {
-            $(".showInfo").append(`${response.data[i].profile.first_name} ${response.data[i].profile.last_name} <br/> ${response.data[i].practices[0].visit_address.street} <br/> ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br/> Phone number: ${response.data[i].practices[0].phones[0].number} <br/> Accepting new patients: ${response.data[i].practices[0].accepts_new_patients} <br/><br/>`);
+            $(".showInfo").append(`<br/><br/>${response.data[i].profile.first_name} ${response.data[i].profile.last_name} <br/> ${response.data[i].practices[0].visit_address.street} <br/> ${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state} <br/> Phone number: ${response.data[i].practices[0].phones[0].number} <br/> Accepting new patients: ${response.data[i].practices[0].accepts_new_patients}`);
+            if (response.data[i].practices[0].website) {
+              $(".showInfo").append(`<br/>${response.data[i].practices[0].website}`);
+            }
           }
         } else {
-          return 1;
+          $(".errors").append("No results");
         }
       }
     });
